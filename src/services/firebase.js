23bc3d0d3ee4a8a7f9966b3c80edfaa7bd211c04
emily <mle4d@me.com>
2019-06-03 16:56:38 -0700

    firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+const config = {
+    apiKey: 'AIzaSyAVv2zqTQWbzOYalWTWqsmVCcu2xYwOloE',
+    authDomain: 'chattycat-c420b.firebaseapp.com',
+    databaseURL: 'https://chattycat-c420b.firebaseio.com',
+    projectId: 'chattycat-c420b',
+    storageBucket: 'chattycat-c420b.appspot.com',
+    messagingSenderId: '13347680511',
+    appId: '1:13347680511:web:2027493e1d1548bd'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
