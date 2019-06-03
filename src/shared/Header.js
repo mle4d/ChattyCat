007ae24dbe5fe04b195278cbef132fc28ad2be98
emily <mle4d@me.com>
2019-06-03 16:43:30 -0700

    header things, header stuff

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -4,18 +4,29 @@ import { auth } from '../services/firebase.js';
 
 class Header extends Component {
 
-}
+    render() {
+        const dom = this.renderDOM();
+        const profile = new Profile();
+        dom.appendChild(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
+
+        return dom;
+    }
 
 
-renderTemplate() {
-    const title = this.props.title || document.title;
+    renderTemplate() {
+        const title = this.props.title || document.title;
 
-    return /*html*/`
-    <header>
-        <h1>${title}</h1>
-        <!-- pRoFiLe HeReEeE -->
-    </header>
-    `;
+        return /*html*/`
+        <header>
+            <h1>${title}</h1>
+            <!-- pRoFiLe HeReEeE -->
+        </header>
+        `;
+    }
 }
 
 export default Header;
