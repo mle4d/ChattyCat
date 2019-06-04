a5f2bd1530f7e34d1430603c091bf58fb3bef75f
emily <mle4d@me.com>
2019-06-03 21:32:38 -0700

    auth stuff

---
+++ src/auth/AuthApp.js
@@ -0,0 +1,40 @@
+import Component from '../Component.js';
+import Header from '../shared/Header.js';
+import { auth } from '../services/firebase.js';
+
+const ui = new firebaseui.auth.AuthUI(auth);
+
+class AuthApp extends component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header({ title: sigh up here });
+        const main = dom.querySelector('main');
+        dom.inserBefore(header.render(), main);
+
+        ui.start('#firebaseui-auth-container', {
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            ],
+            signInSUccessUrl: './',
+            credentialHelper: firebaseui.authCredentialHelper.NONE
+        });
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main>
+                    <p>Sign in to chat about cats!</p>
+                        <!--firebaseui goeseth hereth -->
+                </main>
+            </div>
+            `;
+    }
+}
+
+export default AuthApp;
