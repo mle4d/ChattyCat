8687c2136c74402c1bd9152013a4fdd229c6e640
emily <mle4d@me.com>
2019-06-05 13:30:14 -0700

    chat things, things of chat

---
+++ chat/chat.js
@@ -0,0 +1,14 @@
+import Component from '../src/Component.js';
+
+class component extends Component {
+
+}
+
+
+renderTemplate() {
+    return /*html*/`
+    <fieldset>Chat Rooms
+    <label>Chat: <input name=""></label>
+    </fieldset>
+    ` 
+}
--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,6 @@
 import '../utils/check-auth.js';
 import App from './App.js';
+import chat from '../chat/'
 
 const root = document.getElementById('app');
 const app = new App();
